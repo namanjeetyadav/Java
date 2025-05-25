9e60d01aca0b6bc62a612b5cc9ab011c6f0b74a2
namanjeet <namanshuttler@gmail.com>
2025-05-25 15:44:48 +0530

    java 25.05.25

---
+++ Hello.java
@@ -0,0 +1,23 @@
+class Hello {
+    public static void main(String a[]) {
+
+        int num1 = 1;
+        int num2 = 9;
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println(11 + 3);
+
+        int num3 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+        
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = '8';
+
+        boolean b = true;
+
+    }
+}
