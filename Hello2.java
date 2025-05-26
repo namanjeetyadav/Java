7c29bc475d59a7607a1fecc72484102709332061
namanjeet <namanshuttler@gmail.com>
2025-05-26 16:53:01 +0530

    java 26.05.25

---
+++ Hello2.java
@@ -0,0 +1,22 @@
+public class Hello2 {
+    public static void main(String arg[]){
+        // byte b = 127;
+      int a = 128;
+       byte c = (byte)a;
+       System.out.println(c);
+
+
+       byte a1 = 1;
+       int b1 = a1;
+       System.out.println(b1);
+
+
+    //    Type promotion
+
+    byte a2 = 10;
+    byte b2 = 30;
+
+    int result = a2 * b2;
+    System.out.println(result);
+    }
+}
---
+++ Hello3.java
@@ -0,0 +1,18 @@
+public class Hello3 {
+    public static void main(String a[]) {
+        int n = 5;
+        int result = 0;
+
+        if(n%2==0){
+            result = 10;
+        }
+
+        else {
+                result = 20;
+        }
+
+        System.out.println("Result: " + result);
+
+    }
+
+}
