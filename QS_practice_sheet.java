55aa5b16f7d4cd3361aa68501a2c49e2850f0fc8
namanjeet <namanshuttler@gmail.com>
2025-06-07 03:08:52 +0530

    07.06.25

---
+++ QS_practice_sheet.java
@@ -0,0 +1,133 @@
+public class QS_practice_sheet{
+public static void main(String[] args){
+
+// Java Practice questions on functions
+// 1.Write a Function to calculate addition of 2 numbers
+// int number1 = 10;
+// int number2 = 20;
+// int sum = number1 + number2;
+// System.out.println(sum);
+
+// 2. Write a Function to calculate area of Circle
+// float radius = 10.2f;
+// final float pi= 3.14f; 
+// float area = pi * radius * radius;
+// System.out.println(area);
+
+// 3. Write a Function to calculate area of Triangle
+
+// float base = 10f;
+// float height = 20f;
+// float area_of_triangle = 1.0f/2.0f * base * height;
+// System.out.println(area_of_triangle);
+
+
+// 4. Write a Function to calculate area of Rectangle
+// float length = 10f;
+// float width = 20f;
+// float area_of_rectangle = length * width;
+// System.out.println(area_of_rectangle);
+
+
+// 5. Write a Function to calculate area of square
+// float side = 20;
+// float area_of_square = side * side;
+// System.out.println(area_of_square);
+
+
+// 6. Write a Function to calculate area of cube
+// float side_of_cube = 15f;
+// float area_of_cube = 6f * side_of_cube * side_of_cube;
+// System.out.println(area_of_cube);
+
+// 7. Write a Function to calculate area of Parallelogram
+// float base = 5f;
+// float height = 10f;
+// float area_of_parallelogram = base * height;
+// System.out.println(area_of_parallelogram);
+
+// 8. Write a Function to calculate surface area of Sphere
+// final float pi = 3.14f;
+// float radius = 10;
+// float surface_area = 4f * pi * radius * radius ;
+// System.out.println(surface_area);
+
+// 9. Write a Function to calculate surface area of Cone
+
+
+
+
+
+// 10. Write a Function to calculate volume of cone
+// final float pi = 3.14f;
+// float radius = 10f;
+// float height = 15f;
+// float volume_of_cone  = 1f/3f*pi* radius * radius * height;
+// System.out.println(volume_of_cone);  
+
+
+// 11. Write a Function to calculate volume of Cylinder
+
+// final float pi = 3.14f;
+// float radius = 10f;
+// float height = 15f;
+// float volume_of_cylinder  = pi* radius * radius * height;
+// System.out.println(volume_of_cylinder);  
+    
+
+
+// 12. Write a Function to calculate area of Rhombus
+
+// float height = 15f;
+// float base = 13f;
+// float area_of_rhombus  = base * height;
+// System.out.println(area_of_rhombus);  
+
+
+// 13. Write a Function to calculate area of Pentagon
+
+
+
+
+// 14. Write a Function to calculate area of semi-circle
+// final float pi = 3.14f;
+// float radius = 7f;
+// float area_of_semicircle  = 1f/2f * pi* radius * radius;
+// System.out.println(area_of_semicircle);  
+
+
+// 15. Write a Function to calculate Perimeter of Circle
+
+// final float pi = 3.14f;
+// float radius = 7f;
+// float perimeter_of_circle  = 2f * pi* radius;
+// System.out.println(perimeter_of_circle);
+
+
+// 16. Write a Function to calculate perimeter of semi-circle
+final float pi = 3.14f;
+float radius = 7f;
+float perimeter_of_semicircle  = 2f * pi* radius;
+System.out.println(perimeter_of_semicircle);
+
+
+
+
+// 17. Write a Function to calculate total-surface area of Cube
+// 18. Write a Function to calculate total surface area of Cuboid
+// 19. Write a Function to calculate total surface area of Hemi-sphere
+// 20. Write a Function to calculate square and cube of a number
+// 21. Write a Function to calculate Circumference of Circle
+// 22.Write a Function to calculate km to miles
+// 23. Write a Function to calculate to calculate kg to pound
+// 24. Write a Function to calculate simple interest 
+// 25. Write a Function to calculate total percentage w.r.t 4 subject
+// Where max marks is 80 for each subject
+// 26. Write a Function to calculate area of Trapezoid
+
+
+// 1. 
+
+
+
+}}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+
+final float pi = 3.14f;
+float radius = 7f;
+float perimeter_of_semicircle  = 2f * pi* radius;
+System.out.println(perimeter_of_semicircle);
