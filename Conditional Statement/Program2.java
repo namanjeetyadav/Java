a1d398ab101f818a20b3903ca9b5ef411d7bd34a
namanjeet <namanshuttler@gmail.com>
2025-06-11 15:37:33 +0530

    11.06.25

---
+++ Conditional Statement/Program2.java
@@ -0,0 +1,13 @@
+class Program2{
+    public static void main(String[] args) {
+        System.out.println("program started");
+        int num =8;
+        if (num>=0 && num<=7) {
+            System.out.println("This is a octal number");
+        }
+        else{
+            System.out.println("This is not a octal number");
+        }
+        System.out.println("Program ended");
+    }
+}
