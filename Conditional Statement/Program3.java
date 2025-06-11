3b2951af590612c6282d8316e733d4734dd5754d
namanjeet <namanshuttler@gmail.com>
2025-06-11 15:41:28 +0530

    11.06.25 B

---
+++ Conditional Statement/Program3.java
@@ -0,0 +1,16 @@
+class Program3{
+    public static void main(String[] args) {
+        System.out.println("program started");
+        int num =8;
+        if (num < 7) {
+            System.out.println("Number is less than 7");
+        }
+        else if(num == 7) {
+            System.out.println("Number is equal to 7");
+        }
+        else{
+            System.out.println("Number is greater than 7");
+        }
+        System.out.println("Program ended");
+    }
+}
