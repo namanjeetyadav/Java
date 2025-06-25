ae75ed5414f3d8ace7ef75484a0d4c8673050260
namanjeet <namanshuttler@gmail.com>
2025-06-25 15:03:19 +0530

    25.06.25

---
+++ Methods/Self_practice.java
@@ -0,0 +1,36 @@
+//1.Write a method to check if a number is even or odd.
+//2.Write a method to reverse a string.
+//3.Write a method to find the maximum of three numbers.
+//4.Write a method to count vowels in a string.
+//5.Write a method to calculate factorial of a number.
+
+// Program1
+// class Self_practice {
+
+//     public static void main(String[] args) {
+//         int num = 11;
+//         if (num % 2 == 0) {
+//             System.err.println("This is a even number");
+//         } else {
+//             System.err.println("This is a odd number");
+//         }
+
+//     }
+// }
+
+class Self_practice {
+
+    public static void main(String[] args) {
+       String name = "lets do it";
+       int arlen = name.length();
+       char[] arrayChar = new char[arlen];
+       for (int i =arlen-1; i>=0; i--) {
+           char character = name.charAt(i);
+           arrayChar[arlen - 1- i] = character;
+
+       }
+           System.out.println(arrayChar);
+    }
+}
+
+
