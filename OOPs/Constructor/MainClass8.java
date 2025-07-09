3fc0de6a0f0f38174cb2a2ae1b04117c0b04f10b
namanjeet <namanshuttler@gmail.com>
2025-07-09 16:04:24 +0530

    09.07.25

---
+++ OOPs/Constructor/MainClass8.java
@@ -0,0 +1,25 @@
+class account{
+int account_no;
+String name;
+double account_balance;
+
+account(String name, double account_balance){
+    this.account_no = (int)(Math.random()*100000);
+    this.name = name;
+    this.account_balance = account_balance; 
+}
+
+
+
+void details(){
+System.out.println("Account number is " + this.account_no + " Name " + this.name + "Account balance " + account_balance);
+}
+
+}
+
+class MainClass8{
+    public static void main(String[] args) {
+        account ac1 = new account("Naman", 45666);
+        ac1.details();
+    }
+}
